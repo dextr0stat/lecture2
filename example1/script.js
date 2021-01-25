75d9ec9b295de81458c85ae5f0d492eb7488790e
dextr0stat <77321060+dextr0stat@users.noreply.github.com>
2021-01-26 02:45:14 +0530

    Update script.js

--- example1/script.js
+++ example1/script.js
@@ -2,9 +2,9 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.124.0/build/three.module.js'
 
 // uncomment to load controls
-// import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/controls/OrbitControls.js'
 
-// create a scene and a camera
+// create a scene and a camera, these aee constant. they will not change
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
 camera.position.z = 30
@@ -15,12 +15,12 @@ renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
 // Uncomment next line to add controls
-// const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls( camera, renderer.domElement );
 
 // Create an object and add it to the scene:
 
 // 1. Create the geometry:
-const geometry = new THREE.TorusKnotBufferGeometry( 10, 3, 100, 16 )
+const geometry = new THREE.TorusKnotBufferGeometry( 4, 3, 100, 16 )
 
 // 2. Create the material:
 const material = new THREE.MeshNormalMaterial()
